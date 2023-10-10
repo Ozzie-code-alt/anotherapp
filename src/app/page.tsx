b3fde525b34fd063e9a0c0e527d123eccba4b740
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-10-10 12:01:48 +0800

    added map to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import { prisma } from "@/db";
 import { get } from "http";
 import Link from "next/link";
 import { todo } from "node:test";
+import TodoItem from '@/components/Todo'
+
+
 
 const getTodo =()=>{
 return prisma.todo.findMany()
@@ -17,7 +20,7 @@ const Home = async () => {
         <Link className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" href="/new" >New</Link>
     </header>
       <ul className="pl-4">
-        {todos.map(todo=> <TodoItem key={todo.id} {...todo}/></li>)} 
+        {todos.map(todo=> <TodoItem key={todo.id} {...todo}/>)} 
 
         
       </ul>
